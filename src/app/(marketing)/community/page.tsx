b0d56ea49636c27837787c142c8247a1d9db7912
Abdullah-Al-Akash <aabdullahalakash@gmail.com>
2025-03-17 23:21:03 +0600

    Update

--- src/app/(marketing)/community/page.tsx
+++ src/app/(marketing)/community/page.tsx
@@ -1,14 +1,21 @@
-import { Community } from '@/components/pages/community/community';
-import React from 'react';
+import TitleHeader from "@/components/common/TitleHeader";
+import { Community } from "@/components/pages/community/community";
+import React from "react";
 export const metadata = {
-    title: "Community"
-}
+  title: "Community",
+};
 const page = () => {
-    return (
-        <div>
-            <Community></Community>
-        </div>
-    );
+  return (
+    <div
+      className="w-full"
+      style={{ backgroundImage: "url('/images/overlay/about-overlay.png')" }}
+    >
+      <div className="container mx-auto px-4 py-8 space-y-12 md:space-y-20">
+        <TitleHeader title="Post Our Community" />
+        <Community></Community>
+      </div>
+    </div>
+  );
 };
 
 export default page;
